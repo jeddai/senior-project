1e7a7554171630ef8a095b59f29021a989517e12
Charles Humphreys <ahumphreys3@gmail.com>
2017-02-01 15:45:56 -0600

    More stuff

---
+++ python/test.py
@@ -0,0 +1,5 @@
+print('hello world!')
+x = 0;
+for num in range(1,10):
+    x += num
+print(x);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var PythonShell = require('python-shell');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  PythonShell.run('python/test.py', {}, function(err, results) {
+    if (err) throw err;
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', results);
+    return res.render('index', {
+      title: 'Express',
+      results: results
+    });
+  });
 });
 
 module.exports = router;
